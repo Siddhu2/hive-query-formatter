8223ff884542db1750cc1470f241b78bdae43dc0
Siddharth Murugan <mksiddhu2@gmail.com>
2020-08-01 11:01:08 +0530

    my first commit

---
+++ file_edit.py
@@ -0,0 +1,16 @@
+import os
+import re
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
+my_file = os.path.join(THIS_FOLDER, 'sample.txt')
+text_file = open("Output.txt", "w")
+
+with open(my_file,'r') as file:
+    q = file.read()
+
+edited = " ".join(q.split())
+edited = re.sub(r'\s*,\s*', ',', edited)
+text_file.write(edited)
+text_file.close()
+print (edited)
